84243519d48677f8e06e705877acd48ad6888dad
Longchamp <glongchamp8@gmail.com>
2019-02-26 15:05:10 +0100

    add carrierwave to avatar model and add avatar upload to form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :orders
 
+  mount_uploader :avatar, AvatarUploader
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
+
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
 end

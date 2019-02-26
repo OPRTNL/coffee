9c9045b4ab58a8cbcd97bdb3068fe21f9ca9f33a
OPRTNL <berger.pro.maxime@gmail.com>
2019-02-26 18:25:13 +0100

    done with routes and validation

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,5 +1,9 @@
 class Partner < ApplicationRecord
   belongs_to :user
   geocoded_by :address
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :picture_1, presence: true
+  has_many :orders, through: :products
   after_validation :geocode, if: :will_save_change_to_address?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
 
-  resources :products, only: [:index, :show]
-
-  resources :partners
-
-  resources :users, only: [:show, :edit, :update]
+  resources :partners do
+    resources :products, only: :show
+  end
 
   resources :orders, only: [:show, :create] do
     resources :payments, only: [:new, :create]
   end
+
+  resources :users, only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

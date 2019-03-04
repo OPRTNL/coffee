ca9b1de7e8cddbd81a167d54b1733a04cdbb9814
OPRTNL <berger.pro.maxime@gmail.com>
2019-03-04 09:35:40 +0100

    logger OK, modified link to new_partner in navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :partner]
+  skip_before_action :authenticate_user!, only: [:home, :partners]
 
   def home
   end
 
-  def partner
+  def partners
   end
 end
--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,6 +1,7 @@
 class PartnersController < ApplicationController
   before_action :set_partner, only: [:show, :edit, :update, :destroy]
-  before_action :set_user
+  before_action :set_user, only: [:new, :create, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:city].present?

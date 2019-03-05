881cd162f19b3d74c37563c6d6fc9a16936e82d4
OPRTNL <berger.pro.maxime@gmail.com>
2019-03-05 15:59:03 +0100

    marker toggle OK

--- app/javascript/plugins/init_map.js
+++ app/javascript/plugins/init_map.js
@@ -16,15 +16,17 @@ function initMap() {
   const markerelements          = JSON.parse(mapElement.dataset.markers)
   const markerBounds            = new google.maps.LatLngBounds()
   map                           = new google.maps.Map(mapElement)
-  const iconBase                = 'https://image.flaticon.com/icons/png/256/1079/'
+  const iconBase                = 'https://image.flaticon.com/icons/png/256/'
   let   currentOpenedInfoWindow = null
 
   markerelements.forEach((markerData) => {
     const infowindow  = new google.maps.InfoWindow({content: markerData.infoWindow})
-    const marker      = new google.maps.Marker({position: markerData , map: map, icon: {url: iconBase + '1079115.png', scaledSize: new google.maps.Size(70,70)}})
+    const marker      = new google.maps.Marker({position: markerData , map: map, icon: {url: iconBase + '1079/1079115.png', scaledSize: new google.maps.Size(70,70)}})
 
-    console.log({"id": markerData.partnerId, "marker": marker})
-    markers.push(marker)
+
+
+    // console.log(marker)
+    markers.push({"partnerId": markerData.partnerId, "marker": marker})
     // console.log(markers)
     markerBounds.extend(new google.maps.LatLng(markerData))
 
@@ -44,12 +46,22 @@ function initMap() {
     map.setZoom(16)
   }
 }
+// console.log(markers)
+let currentMarker = null
 
 const focusOnMarker = (partnerId) => {
-  console.log(`je dois focus sur le parter ${partnerId}`)
-  console.log(partnerId)
-  console.log("map", map)
-  console.log("markers", markers)
+
+  const iconBase = 'https://image.flaticon.com/icons/png/256/'
+
+  const marker = markers.find((element) => { return element.partnerId == partnerId }).marker
+  console.log(marker)
+  marker.setIcon({url: iconBase + '252/252025.png', scaledSize: new google.maps.Size(70,70)})
+  if (currentMarker != null) {
+    currentMarker.setIcon({url: iconBase + '1079/1079115.png', scaledSize: new google.maps.Size(70,70)})
+  }
+  currentMarker = marker
+  map.panTo(marker.position)
+  map.setZoom(15)
 }
 
 

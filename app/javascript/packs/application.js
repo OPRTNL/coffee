70ee8ef61767a76a03b30c817bcde99591e2e953
OPRTNL <berger.pro.maxime@gmail.com>
2019-03-05 11:57:05 +0100

    map fixed new focus on selected partner on the way

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,13 @@
 import "bootstrap";
 import initAutocomplete from "../plugins/init_autocomplete.js"
 import initLocation from "../plugins/init_location.js"
-import initMap from "../plugins/init_map.js"
+import { initMap, focusOnMarker } from "../plugins/init_map.js"
 import initToogle from "../plugins/init_toogle.js"
 
 // // console.log('Hello World from Webpacker');
 initAutocomplete();
 initLocation();
-initToogle();
+initToogle(focusOnMarker);
 window.initMap = initMap
 
 

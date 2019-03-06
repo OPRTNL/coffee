e6e6898f86031ba086d3561050a399caffff966d
Deborah <heima.heima.creative@gmail.com>
2019-03-06 15:14:14 +0100

    edit edit edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,17 +22,17 @@ beers = JSON.parse(coffees)["resultat"]
 puts "Seeding"
 print "..."
 
-user_1 = User.create!(first_name: "Laurent", last_name: "Korvisy", email: "laurent.korvisy@gmail.com", password: "lknd2kd1", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110119/peoples/adult-beard-blur-713520.jpg")
-user_2 = User.create!(first_name: "Thomas", last_name: "Morin", email: "thomas.morin@gmail.com", password: "lknd2kdf", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110146/peoples/adult-bald-blur-1408196.jpg")
-user_3 = User.create!(first_name: "Charlotte", last_name: "Goyard", email: "charlotte.goyard@gmail.com", password: "lknd2kdg", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110122/peoples/alone-beautiful-blurred-background-720598.jpg")
-user_4 = User.create!(first_name: "Maxime", last_name: "Carneiro", email: "maxime.carneiro@gmail.com", password: "lknd2kdy", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110118/peoples/adult-beach-beard-736716.jpg")
-user_5 = User.create!(first_name: "Calvin", last_name: "Ponsot", email: "calvin.ponsot@gmail.com", password: "lknd2kdy", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110118/peoples/adult-beach-beard-736716.jpg")
+user_1 = User.create!(first_name: "Laurent", last_name: "Korvisy", email: "laurent.korvisy@gmail.com", password: "lknd2kd1", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110119/peoples/adult-beard-blur-713520.jpg")
+user_2 = User.create!(first_name: "Thomas", last_name: "Morin", email: "thomas.morin@gmail.com", password: "lknd2kdf", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110146/peoples/adult-bald-blur-1408196.jpg")
+user_3 = User.create!(first_name: "Charlotte", last_name: "Goyard", email: "charlotte.goyard@gmail.com", password: "lknd2kdg", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110122/peoples/alone-beautiful-blurred-background-720598.jpg")
+user_4 = User.create!(first_name: "Maxime", last_name: "Carneiro", email: "maxime.carneiro@gmail.com", password: "lknd2kdy", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110118/peoples/adult-beach-beard-736716.jpg")
+user_5 = User.create!(first_name: "Calvin", last_name: "Ponsot", email: "calvin.ponsot@gmail.com", password: "lknd2kdy", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110118/peoples/adult-beach-beard-736716.jpg")
 print ".."
-user_6 = User.create!(first_name: "Rémi", last_name: "Falcoz", email: "remi.falcoz@gmail.com", password: "lknd2kdo", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110755/peoples/beard-casual-confidence-1878687.jpg")
-user_7 = User.create!(first_name: "Chloé", last_name: "Rouby", email: "chloe.rouby@gmail.com", password: "lknd2kdl", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110104/peoples/beautiful-beauty-brown-eyes-1065084.jpg")
-user_8 = User.create!(first_name: "Yasue", last_name: "Nobuyoki", email: "yasue.nobuyoki@gmail.com", password: "lknd2kd2", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110918/peoples/blurred-background-business-cafe-1125028.jpg")
-user_9 = User.create!(first_name: "Joe", last_name: "Wayan", email: "joe.wayan@gmail.com", password: "lknd2kd@", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110758/peoples/beard-expression-face-1211605.jpg")
-user_10 = User.create!(first_name: "Charlène", last_name: "Haskis", email: "charlene.haskis@gmail.com", password: "lknd2kd!", phone_number:"06 08 09 09 09", profile_picture:"https://res.cloudinary.com/di985lta5/image/upload/v1551110102/peoples/blur-casual-coffee-712513.jpg")
+user_6 = User.create!(first_name: "Rémi", last_name: "Falcoz", email: "remi.falcoz@gmail.com", password: "lknd2kdo", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110755/peoples/beard-casual-confidence-1878687.jpg")
+user_7 = User.create!(first_name: "Chloé", last_name: "Rouby", email: "chloe.rouby@gmail.com", password: "lknd2kdl", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110104/peoples/beautiful-beauty-brown-eyes-1065084.jpg")
+user_8 = User.create!(first_name: "Yasue", last_name: "Nobuyoki", email: "yasue.nobuyoki@gmail.com", password: "lknd2kd2", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110918/peoples/blurred-background-business-cafe-1125028.jpg")
+user_9 = User.create!(first_name: "Joe", last_name: "Wayan", email: "joe.wayan@gmail.com", password: "lknd2kd@", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110758/peoples/beard-expression-face-1211605.jpg")
+user_10 = User.create!(first_name: "Charlène", last_name: "Haskis", email: "charlene.haskis@gmail.com", password: "lknd2kd!", phone_number:"06 08 09 09 09", photo:"https://res.cloudinary.com/di985lta5/image/upload/v1551110102/peoples/blur-casual-coffee-712513.jpg")
 
 print "....."
 
